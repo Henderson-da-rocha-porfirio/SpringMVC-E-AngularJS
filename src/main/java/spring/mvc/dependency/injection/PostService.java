194cee5010dca1c357ad58be442221c53aa5592f
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-02-20 18:30:01 -0300

    Delete src directory

--- src/main/java/spring/mvc/dependency/injection/PostService.java
+++
@@ -1,6 +0,0 @@
-package spring.mvc.dependency.injection;
-
-public interface PostService {
-	public boolean publishPost(String content);
-
-}
--- src/main/java/spring/mvc/quartz/CronQuartzTask.java
+++
@@ -1,9 +0,0 @@
-package spring.mvc.quartz;
-import java.util.Calendar;
-
-public class CronQuartzTask {	
-	 public void cronTaskMethod() {		
-		//you can send emails to users here
-		//System.out.println("Cron Time: " + Calendar.getInstance().getTime());
-     }
-}
